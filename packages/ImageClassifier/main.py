0cb6e775a58208b64f6a19acb668eacb2be319f3
Uberwald <106376063+Uberwald@users.noreply.github.com>
2023-09-09 09:39:02 +0300

    Update main.py

--- packages/ImageClassifier/main.py
+++ packages/ImageClassifier/main.py
@@ -8,10 +8,9 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = clip.load("RN50x64", device=device)
 
 # список классов, можно свободно добавлять любые
-labels_prod = ['frozen foods', 'perfumery and cosmetic products',
-          'dishes', 'dairy products', 'household goods', 'water, juices, drinks']
-labels_not_prod = []
-labels_excep = []
+labels_prod = ['frozen foods', 'dairy products', 'water, juices, drinks']
+labels_not_prod = ['dishes', 'household goods']
+labels_excep = ['perfumery and cosmetic products', 'household chemicals']
 labels = labels_prod + labels_not_prod + labels_excep
 
 text = clip.tokenize(labels).to(device)
@@ -36,10 +35,21 @@ def image_recogn(image_path):
     probs = dict(zip(labels, probs[0]))
     sorted_items = sorted(probs.items(), key=lambda x: x[1], reverse=True)
 
-    # Если процент уверенности слишком мал, класс = 0
-    if sorted_items[0][1] < 0.2:
-        product_class = 0
-
     # Определение класса продукта
-    product_class = class_dict[sorted_items[0][0]]
-    return product_class
+    if sorted_items[0][0] in labels_prod:
+      product_class = 0
+    elif sorted_items[0][0] in labels_not_prod:
+      product_class = 1
+    elif sorted_items[0][0] in labels_excep:
+      product_class = 2
+
+    # Если процент уверенности слишком мал, класс = 3
+    if sorted_items[0][1] < 0.2:
+        product_class = 3
+    answer = [product_class, sorted_items[0][0]]
+
+    return tuple(answer)
+
+def help():
+    print('0 - продовольственные товары\n1 - непродовольственные товары\n2 - товары исключения'
+    '\n3 - товар не распознан')
